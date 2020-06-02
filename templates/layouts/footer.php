ed32521cf87336639aeb1651be2435b2441135c2
Jyoti Shukla <jyoti@Jyotis-MacBook-Air.local>
2020-06-02 08:49:40 +0530

    first commit

--- templates/layouts/footer.php
+++ templates/layouts/footer.php
@@ -3,27 +3,98 @@
     <div class="wrapper">
         <div class="widget-text-area">
             <div class="text-area">
-                template by
-                <a href="http://www.themeforest.net/user/themeexpo">ThemeExpo</a>
-                All rights reserved
+                <h5>We are Sigma</h5>
+
+                <p>Interdum et duis sed fames ac ante ipsum primis in faucibus. Proin velit massa, venenatis quis tinci dunt eu, hendrerit porta mauris molestie lectus eget sit amet dolor duis unde lacina velit quam venenatis.</p>
             </div>
             <div class="social-media-footer">
                 <a href="https://web.facebook.com/" class="" target="_blank">
-                    <i class="fa fa-facebook"></i>
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
                 </a>
                 <a href="https://twitter.com/" class="" target="_blank">
-                    <i class="fa fa-twitter"></i>
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
                 </a>
                 <a href="https://instagram.com/" class="" target="_blank">
-                    <i class="fa fa-instagram"></i>
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
                 </a>
                 <a href="https://pinterest.com/" class="" target="_blank">
-                    <i class="fa fa-pinterest"></i>
+                    <i class="fa fa-pinterest" aria-hidden="true"></i>
                 </a>
                 <a href="https://web.youtube.com/" class="" target="_blank">
-                <i class="fa fa-youtube"></i>
+                    <i class="fa fa-youtube" aria-hidden="true"></i>
                 </a>
             </div>
+        </div>
+        <div class="recent-post-area">
+            <h6>recent post</h6>
+            <div class="image-area-section">
+                <div class="image-area">
+                    <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/3-60x60.jpg" alt="">
+                </div>    
+                <div class="new-trend-area">
+                    <a class="post-title" href="http://wp.efforttech.net/newwp/sigma/2019/01/major-trends-expected-2018-4/">new trends</a>
+                <div class="post-details">
+                    <a class="post-author" href="http://wp.efforttech.net/newwp/sigma/author/admin/">by admin</a>
+                <div class="date-area">   
+                    <a class="post-date" href="http://wp.efforttech.net/newwp/sigma/21/01/">21 Jan</a>
+                </div>
+                </div>
+                </div>
+            </div>
+            <hr>
+            <div class="image-area-section">
+                <div class="image-area">
+                    <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/image-3-7-60x60.jpg" alt="">
+                </div>    
+                <div class="new-trend-area">
+                    <a class="post-title" href="http://wp.efforttech.net/newwp/sigma/2019/01/major-trends-expected-2018-4/">new trends</a>
+                <div class="post-details">
+                    <a class="post-author" href="http://wp.efforttech.net/newwp/sigma/author/admin/">by admin</a>
+                <div class="date-area">   
+                    <a class="post-date" href="http://wp.efforttech.net/newwp/sigma/21/01/">21 Jan</a>
+                </div>
+                </div>
+                </div>
+            </div>
+            <hr>
+            <div class="image-area-section">
+                <div class="image-area">
+                    <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/image-2-8-60x60.jpg" alt="">
+                </div>    
+                <div class="new-trend-area">
+                    <a class="post-title" href="http://wp.efforttech.net/newwp/sigma/2019/01/major-trends-expected-2018-4/">new trends</a>
+                <div class="post-details">
+                    <a class="post-author" href="http://wp.efforttech.net/newwp/sigma/author/admin/">by admin</a>
+                <div class="date-area">   
+                    <a class="post-date" href="http://wp.efforttech.net/newwp/sigma/21/01/">21 Jan</a>
+                </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="useful-links-area">
+        <h5>useful links</h5>
+        </div>
+        <div class="useful-links">
+        <ul>
+			<li class="page_item page-item-488">
+                <a href="http://wp.efforttech.net/newwp/sigma/about-us/">About Us</a>
+            </li>
+            <li class="page_item page-item-309">
+                <a href="http://wp.efforttech.net/newwp/sigma/blog-post-right-sidebar/">Blog post right sidebar</a>
+            </li>
+            <li class="page_item page-item-428">
+                <a href="http://wp.efforttech.net/newwp/sigma/coming-soon/">Coming Soon</a>
+            </li>
+            <li class="page_item page-item-7 current_page_item"><a href="http://wp.efforttech.net/newwp/sigma/">Home demo 01</a>
+            </li>
+            <li class="page_item page-item-6">
+                <a href="http://wp.efforttech.net/newwp/sigma/home-demo-02/">Home demo 02</a>
+            </li>
+            <li class="page_item page-item-683">
+                <a href="http://wp.efforttech.net/newwp/sigma/shop/">Shop</a>
+            </li>
+		</ul>
         </div>
     </div>
 </div>
--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -1,36 +1,38 @@
 <header>
-	<div class="header-logo">
-		<img src="assets/images/header/site-logo.png" alt="Site logo">
-	</div>
-	<nav>
-		<ul class="nav-wrap">
-			<li class="nav-list-item">
-				<a href="#">Home</a>
-			</li>
-			<li class="nav-list-item">
-				<a href="#">Services</a>
-			</li>
-			<li class="nav-list-item">
-			<a href="#">portfolio</a>
-			</li>
-			<li class="nav-list-item">
-			<a href="#">about us</a>
-			</li>
-			<li class="nav-list-item">
-			<a href="#">blog</a>
-			</li>
-			<li class="nav-list-item">
-			<a href="#">partners</a>
-			</li>
-			<li class="nav-list-item">
-			<a href="#">pages</a>
-			</li>
-			<li class="nav-list-item">
-			<a href="#">contacts</a>
-			</li>
-		</ul>
-		<div class="quote-btn">
-			<a href="#">Get a Quote</a>
+	<div class="wrapper">
+		<div class="header-logo">
+			<img src="assets/images/header/site-logo.png" alt="Site logo">
 		</div>
-	</nav>
-</header>
+		<nav>
+			<ul class="nav-wrap">
+				<li class="nav-list-item">
+					<a href="#">Home</a>
+				</li>
+				<li class="nav-list-item">
+					<a href="#">Services</a>
+				</li>
+				<li class="nav-list-item">
+				<a href="#">portfolio</a>
+				</li>
+				<li class="nav-list-item">
+				<a href="#">about us</a>
+				</li>
+				<li class="nav-list-item">
+				<a href="#">blog</a>
+				</li>
+				<li class="nav-list-item">
+				<a href="#">partners</a>
+				</li>
+				<li class="nav-list-item">
+				<a href="#">pages</a>
+				</li>
+				<li class="nav-list-item">
+				<a href="#">contacts</a>
+				</li>
+			</ul>
+			<div class="quote-btn">
+				<a href="#">Get a Quote</a>
+			</div>
+		</nav>
+	</div>
+</header>	
