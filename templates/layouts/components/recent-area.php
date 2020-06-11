ec9195ca6272c5f24f8fc6e7ce11397c6ab57a74
Jyoti Shukla <jyoti@Jyotis-MacBook-Air.local>
2020-06-11 13:29:55 +0530

    image link add

--- templates/layouts/components/recent-area.php
+++ templates/layouts/components/recent-area.php
@@ -2,7 +2,7 @@
                 <h6>recent post</h6>
                 <div class="image-area-section">
                     <div class="image-area">
-                        <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/3-60x60.jpg" alt="">
+                        <img src="../../../assets/images/footer-section-one/3-60x60.jpg" alt="">
                     </div>    
                     <div class="new-trend-area">
                         <a class="post-title" href="http://wp.efforttech.net/newwp/sigma/2019/01/major-trends-expected-2018-4/">new trends</a>
