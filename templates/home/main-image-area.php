5090bd36209bbbd5ffc571c26a64c5cfe811bdd9
Jyoti Shukla <jyoti@Jyotis-MacBook-Air.local>
2020-06-19 05:43:56 +0530

    image grid section update

--- templates/home/main-image-area.php
+++ templates/home/main-image-area.php
@@ -1,7 +1,37 @@
 <section class="portfolio">
     <div class="portfolio-items">
         <div class="portfolio-item-thumbnail">
-            <img src="/assets/images/sigma/image-bottle.jpg" alt="">
+                <!-- thumbnai section -->
+            <div class="image-area-thumbnail-section">
+                <!-- image area section one -->
+                <div class="image-section-one">
+                    <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/image-9.jpg" alt="">
+                </div>
+                <!-- image area section two -->
+                <div class="image-section-two">
+                    <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/image-2-1.jpg" alt="">
+                </div>
+                <!-- image area section three -->
+                <div class="image-section-three">
+                    <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/image-1-1.jpg" alt="">
+                </div>
+                <!-- image area section four -->
+                <div class="image-section-four">
+                    <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/image-3-1.jpg" alt="">
+                </div>
+                <!-- image area section five -->
+                <div class="image-section-five">
+                    <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/image-4.jpg" alt="">
+                </div>
+                <!-- image area section six -->
+                <div class="image-section-six">
+                    <img src="http://wp.efforttech.net/newwp/sigma/wp-content/uploads/2019/01/image-10.jpg" alt="">
+                </div>
+                <!-- image area section seven-->
+                <div class="image-section-seven">
+                    <img src="assets/images/sigma-images/image-coffee.jpg" width="375" height="190" alt="Coffee mug">
+                </div>
+            </div>
         </div>
     </div>
 </section>
